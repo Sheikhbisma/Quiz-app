d973016593993f78356db95a9eb7bdb6e013e90c
Sheikhbisma <bismasheikh2006@gmail.com>
2026-08-30 20:38:11 +0500

    okssssss

--- api/index.php
+++ api/index.php
@@ -1,14 +1,16 @@
 <?php
 
-try {
-    require __DIR__ . '/../public/index.php';
-} catch (\Throwable $e) {
-    http_response_code(500);
+$storagePath = '/tmp/storage';
+$cachePath = '/tmp/bootstrap/cache';
 
-    echo '<pre>';
-    echo "ERROR: " . $e->getMessage() . "\n\n";
-    echo "FILE: " . $e->getFile() . "\n";
-    echo "LINE: " . $e->getLine() . "\n\n";
-    echo $e->getTraceAsString();
-    echo '</pre>';
+if (!is_dir($storagePath)) {
+    mkdir($storagePath, 0755, true);
 }
+
+if (!is_dir($cachePath)) {
+    mkdir($cachePath, 0755, true);
+}
+
+putenv("LARAVEL_STORAGE_PATH=$storagePath");
+
+require __DIR__ . '/../public/index.php';
