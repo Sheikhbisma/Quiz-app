750ec3de7eba236a422d32f07b2ac23b8dab1d2f
Sheikhbisma <bismasheikh2006@gmail.com>
2026-08-30 20:43:44 +0500

    patience

--- api/index.php
+++ api/index.php
@@ -1,16 +1,11 @@
 <?php
 
-$storagePath = '/tmp/storage';
-$cachePath = '/tmp/bootstrap/cache';
+require __DIR__ . '/../vendor/autoload.php';
 
-if (!is_dir($storagePath)) {
-    mkdir($storagePath, 0755, true);
-}
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
-if (!is_dir($cachePath)) {
-    mkdir($cachePath, 0755, true);
-}
+$app->useStoragePath('/tmp');
 
-putenv("LARAVEL_STORAGE_PATH=$storagePath");
-
-require __DIR__ . '/../public/index.php';
+$app->handleRequest(
+    \Illuminate\Http\Request::capture()
+);
