998f9fd85a6e3aaba87d85966a6389931e65d9b4
Sheikhbisma <bismasheikh2006@gmail.com>
2026-08-30 20:33:12 +0500

    oksssss

--- api/index.php
+++ api/index.php
@@ -1,3 +1,14 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    http_response_code(500);
+
+    echo '<pre>';
+    echo "ERROR: " . $e->getMessage() . "\n\n";
+    echo "FILE: " . $e->getFile() . "\n";
+    echo "LINE: " . $e->getLine() . "\n\n";
+    echo $e->getTraceAsString();
+    echo '</pre>';
+}
